bdf66935f60fa7276e13faa9711c3f7222369822
mvgiacomello <mvgiacomello@gmail.com>
2019-03-25 13:48:53 +0000

    O(n) solution for longest substring.

--- longest_substring_wo_repeat_chars/solution.py
+++ longest_substring_wo_repeat_chars/solution.py
@@ -5,10 +5,35 @@ def length_longest_substring(full_string: str) -> int:
     :return: length of the longest substring without repeating characters
     """
     string = full_string.lower()
+
+    # Check edge-cases
     if len(string) == 0:
         return 0
     elif len(string) == 1:
         return 1
-    else:
-        # TODO: implement me
-        return -1
+
+    # Check the rest of the scenarios
+    longest = 1
+    for start in range(0, len(string) - 1):
+        found = False
+        for end in range(start + 1, len(string)):
+            sub_string = string[start:end + 1]
+            if not repeat_chars(sub_string) and len(sub_string) > longest:
+                longest = len(sub_string)
+                found = True
+            elif found:
+                break
+    return longest
+
+
+def repeat_chars(substring: str) -> bool:
+    """
+    :param substring: a string to analyze
+    :return: True if has repeating characters False otherwise
+    """
+    string = substring.lower()
+    for start in range(0, len(string)):
+        for end in range(start + 1, len(string)):
+            if string[start] == string[end]:
+                return True
+    return False
--- longest_substring_wo_repeat_chars/test_solution.py
+++ longest_substring_wo_repeat_chars/test_solution.py
@@ -3,16 +3,24 @@ from longest_substring_wo_repeat_chars import solution
 
 
 class TestSolution(unittest.TestCase):
+    def test_repeat_char_detection(self):
+        self.assertTrue(solution.repeat_chars('aa'))
+        self.assertTrue(solution.repeat_chars('aA'))
+        self.assertTrue(solution.repeat_chars('abcabcd'))
+        self.assertFalse(solution.repeat_chars('ab'))
+        self.assertFalse(solution.repeat_chars('abc'))
 
     def test_solution(self):
         self.assertEqual(0, solution.length_longest_substring(''))
         self.assertEqual(1, solution.length_longest_substring('a'))
+        self.assertEqual(2, solution.length_longest_substring('au'))
+        self.assertEqual(1, solution.length_longest_substring('aa'))
+        self.assertEqual(2, solution.length_longest_substring('aab'))
         self.assertEqual(3, solution.length_longest_substring('abcabcbb'))
         self.assertEqual(3, solution.length_longest_substring('abcABCBB'))
         self.assertEqual(1, solution.length_longest_substring('bbbbb'))
         self.assertEqual(3, solution.length_longest_substring('pwwkew'))
         self.assertEqual(6, solution.length_longest_substring('mauricio'))
-
 
 if __name__ == '__main__':
     unittest.main()
