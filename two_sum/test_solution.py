092770d2531fa4264711a77f95f5770b209bef27
Mauricio Giacomello <mvgiacomello@gmail.com>
2019-03-25 11:25:36 +0000

    Reorder tests of two_sum so error messages would print correctly in case of an error.

--- two_sum/test_solution.py
+++ two_sum/test_solution.py
@@ -5,13 +5,13 @@ from two_sum import solution
 class TestSolution(unittest.TestCase):
 
     def test_solution(self):
-        self.assertEqual(solution.two_sum(nums=[2, 7, 11, 3], target=9), [0, 1])
-        self.assertEqual(solution.two_sum(nums=[7, 2, 11, 3], target=9), [0, 1])
-        self.assertEqual(solution.two_sum(nums=[7, 2, 11, 3], target=14), [2, 3])
-        self.assertEqual(solution.two_sum(nums=[-1, -2, 11, 3], target=-3), [0, 1])
-        self.assertEqual(solution.two_sum(nums=[-1, -2, -1, -2], target=-3), [0, 1])
-        self.assertEqual(solution.two_sum(nums=[0, 0, 0, 0], target=0), [0, 1])
-        self.assertEqual(solution.two_sum(nums=[7, 2, 11, 3], target=9), [0, 1])
+        self.assertEqual([0, 1], solution.two_sum(nums=[2, 7, 11, 3], target=9))
+        self.assertEqual([0, 1], solution.two_sum(nums=[7, 2, 11, 3], target=9))
+        self.assertEqual([2, 3], solution.two_sum(nums=[7, 2, 11, 3], target=14))
+        self.assertEqual([0, 1], solution.two_sum(nums=[-1, -2, 11, 3], target=-3))
+        self.assertEqual([0, 1], solution.two_sum(nums=[-1, -2, -1, -2], target=-3))
+        self.assertEqual([0, 1], solution.two_sum(nums=[0, 0, 0, 0], target=0))
+        self.assertEqual([0, 1], solution.two_sum(nums=[7, 2, 11, 3], target=9))
         with self.assertRaises(ValueError):
             solution.two_sum(nums=[7], target=7)
             solution.two_sum(nums=[1, 2, 3], target=8)
