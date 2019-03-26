cdbabd9684e28e01e62fa9140d4c0b5dba40b7f1
Mauricio Giacomello <mauricio.giacomello@touchsurgery.com>
2019-03-26 15:44:01 +0000

    Updated TestHashMap file with more scenarios.

--- helpers/tests/test_hash_map.py
+++ helpers/tests/test_hash_map.py
@@ -4,21 +4,71 @@ from helpers.hash_map import HashMap
 
 
 class TestHashMap(unittest.TestCase):
-    def setUp(self):
-        self.small_map = HashMap(size=2)
+    def test_put_on_small_map(self):
+        self.assertRaises(ValueError, HashMap, -1)
+        self.assertRaises(ValueError, HashMap, 0)
+        self.assertRaises(ValueError, HashMap, 1)
 
-    def test_hash_map(self):
-        self.small_map.put('Orange', 1.5)
-        self.small_map.put('Apple', 2)
-        self.small_map.put('Apple', 4)
-        self.small_map.put('Apple', 6)
-        self.small_map.put('Grape', -10)
-        self.small_map.put(5, 6)
-        self.assertEqual(1.5, self.small_map.get('Orange'))
-        self.assertEqual(6, self.small_map.get('Apple'))
-        self.assertEqual(-10, self.small_map.get('Grape'))
-        self.assertEqual(6, self.small_map.get(5))
-        self.assertIsNone(self.small_map.get('Banana'))
+    def test_put_same_key(self):
+        map = HashMap()
+        map.put('Apple', 'Are good good for you')
+        map.put('Apple', 'Are bad for you')
+        map.put('Apple', 0)
+        self.assertEqual(0, map.get('Apple'))
+
+    def test_put_integer(self):
+        map = HashMap()
+        map.put(-1, -1)
+        map.put(0, 0)
+        map.put(1, 1)
+        self.assertEqual(-1, map.get(-1))
+        self.assertEqual(0, map.get(0))
+        self.assertEqual(1, map.get(1))
+
+    def test_put_string(self):
+        map = HashMap()
+        map.put('Apple', 'Apple')
+        map.put('Orange', 'Orange')
+        map.put('Grape', 'Grape')
+        self.assertEqual('Apple', map.get('Apple'))
+        self.assertEqual('Orange', map.get('Orange'))
+        self.assertEqual('Grape', map.get('Grape'))
+
+    def test_put_other_objects(self):
+        # Dummy class
+        class Test:
+            self.value = 0
+        test_object = Test()
+
+        map = HashMap()
+        map.put(test_object, 'Apple')
+        self.assertEqual('Apple', map.get(test_object))
+
+    def test_put_and_get_different_types(self):
+        map = HashMap(2)
+        map.put('Apple', 'Apple')
+        map.put('Orange', 'Orange')
+        map.put('Grape', 'Grape')
+        map.put(1, 1)
+        map.put(2, 2)
+        map.put(3, 3)
+        self.assertEqual('Apple', map.get('Apple'))
+        self.assertEqual('Orange', map.get('Orange'))
+        self.assertEqual('Grape', map.get('Grape'))
+        self.assertEqual(1, map.get(1))
+        self.assertEqual(2, map.get(2))
+        self.assertEqual(3, map.get(3))
+
+    def test_get_on_empty_map(self):
+        map = HashMap()
+        self.assertIsNone(map.get('Apple'))
+
+    def test_large_with_lots_of_entries(self):
+        map = HashMap(2)
+        map.put('Apple', 'Apple')
+        for index in range(0, 2000):
+            map.put('Apple' + str(index), 'Apple')
+        self.assertEqual('Apple', map.get('Apple'))
 
 
 if __name__ == '__main__':
